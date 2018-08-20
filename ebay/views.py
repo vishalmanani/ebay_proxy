09e1afe6e3f2570f4d7e51d32ddf38fd29164d18
vishalmanani <vishal@lintelindia.com>
2018-08-20 11:21:46 +0530

    change webhook url dev to production

--- ebay/views.py
+++ ebay/views.py
@@ -84,7 +84,7 @@ class EbayNotification(View):
             rdo = res.ResponseDataObject({'content': xml})
             r = res.Response(rdo)
             data = r.json()
-            notification_data = requests.post('http://xb-dev.us-east-2.elasticbeanstalk.com/webhook/notification/', data=data)
+            notification_data = requests.post('http://xb-production.us-east-2.elasticbeanstalk.com/webhook/notification/', data=data)
 
             if notification_data.status_code == 200:
                 response = {
